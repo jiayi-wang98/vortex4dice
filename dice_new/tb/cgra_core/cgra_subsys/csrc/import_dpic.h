55fc4a4b4b78da6b8a62c6569e8bfb429359f9b5
Jiayi Wang <jwang710@uw.edu>
2025-10-14 20:56:20 -0700

    remove sim file

--- dice_new/tb/cgra_core/cgra_subsys/csrc/import_dpic.h
+++
@@ -1,4 +0,0 @@
-
- extern void dice_alu_eval_comb(/* INPUT */long long in0, /* INPUT */long long in1, /* INPUT */long long in2, /* INPUT */long long in3, /* INPUT */unsigned int opcode, /* OUTPUT */long long *out0, /* OUTPUT */long long *out1);
-
- extern void dice_alu_cycle(/* INPUT */long long in0, /* INPUT */long long in1, /* INPUT */long long in2, /* INPUT */long long in3, /* INPUT */unsigned int opcode, /* OUTPUT */long long *out0, /* OUTPUT */long long *out1);
