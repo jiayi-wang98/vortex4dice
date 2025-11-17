a8c80c9d937d320e46c76edb3fcaaf0346286e90
Elliot Norman <elliotjnorman@gmail.com>
2025-11-17 00:20:20 -0800

    bug fixes in valid check - needs testing (ensure that the prefetch block, unresolved divergence, and barrier logic works and that interaction with decoder is as intended

--- dice_new/rtl/cgra_core/fetch_stage/valid_check.sv
+++ dice_new/rtl/cgra_core/fetch_stage/valid_check.sv
@@ -1,9 +1,11 @@
 /*
-Need to learn more about barrier condition and what that means for the validity,
-need to figure out how decoder and branch handler should tell module if they are done
+CONDITIONS FOR VALID TO BE ASSERTED:
+1) Bitstream Loaded
+2) E-block prefetch cleared or not prefetch block
+3) Valid mask 
+4) Barrier condition met
 */
-
-
+ //TO DO: Ensure that the prefetch and unresolved divergence is correct
 module valid_check #(
     parameter PC_WIDTH = 32
 )(
@@ -12,47 +14,85 @@ module valid_check #(
 
     //from decoder (if it is 1 then all prev blocks must finish before ex this p graph)
     input logic barrier_indicator,
+    input logic mask_valid,
+    output logic valid_ready,
 
     //from CS, FDR buffer
     input logic [PC_WIDTH-1:0] eblock_pc,
-
+    input logic prefetch_block,
 
     //from SIMT_Stack
     input logic [PC_WIDTH-1:0] simt_stack_pc, // "next pc"
 
-
-    //from bitstream loader,
-    input logic bitstream_loaded,
-
+    
+    input logic bitstream_valid,
+    output logic bitstream_ready,
 
     //from cta status table
     input logic unresolved_div,
+    input logic barrier_done,
 
 
     //to FDR DE buffer
-    output logic fdr_valid
+    output logic fdr_valid,
+    input logic ex_ready
 );
 
-    logic valid_d, valid_q;
-    logic pc_match;
-    logic barrier_cond_met;
+    //'STATES'
+    logic valid_d, valid_q; 
+
+    //intermediate signals
+    logic pc_match; //if the pc from the simt stack and the pc from schedule match
+    logic prefetch_ok; // if it is either not a prefetch block, or the prefetch condition has been cleared
+    logic bitstream_ok; //if bitstream is loaded
+    logic mask_ok; //if mask is valid
+    logic barrier_ok; //if barrier condition is met
+    logic no_divergence; //MAY NEED TO MODIFY
 
 
-    
+    logic can_issue; // true if all conditions are valid
+    logic is_issued; // asserted when the FDR stage is valid and the EX stage is ready
+
+    assign pc_match = eblock_pc == simt_stack_pc;
+    assign prefetch_ok = !prefetch_block; //NEED TO MODIFY
+    assign bitstream_ok = bitstream_valid;
+    assign mask_ok = mask_valid;
+    assign barrier_ok = barrier_done || (!barrier_indicator);
+    assign no_divergence = !unresolved_div;
+
+    //checks if all conditions are true
+    assign can_issue = pc_match         && 
+                       prefetch_ok      &&
+                       bitstream_ok     &&
+                       barrier_ok       &&
+                       mask_ok          &&
+                       no_divergence;
+
+    assign valid_ready = (!valid_q) || (valid_q && ex_ready); //may not need this -> need to assess decode logic
+    assign is_issued = can_issue && ex_ready;
+    assign fdr_valid = valid_q;
+
+
     always_comb begin
-
-
+        valid_d = valid_q;
+        if (valid_q) begin
+            if (ex_ready) begin
+                if (can_issue) begin
+                    valid_d = 1'b1;
+                end else begin
+                    valid_d = 1'b0;
+                end
+            end else begin
+                valid_d = 1'b1;
+            end
+        end else begin
+            if (can_issue) begin
+                valid_d = 1'b1;
+            end
+        end
     end
 
 
-
-    assign fdr_valid = valid_q;
-    assign pc_match = (eblock_pc == simt_stack_pc); //This 
-
-
-    assign valid_d = (pc_match && bitstream_loaded && !unresolved_div && barrier_cond_met);
-
-    //may need to add more logic for barrier condition
     always_ff @(posedge clk or negedge rst_n) begin
         if(!rst_n) begin
             valid_q <= 1'b0;
@@ -60,5 +100,4 @@ module valid_check #(
             valid_q <= valid_d;
         end
     end
-
 endmodule
