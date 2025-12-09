aa103c0adb52e32ec571bdc5c05a5a029483efab
Elliot Norman <elliotjnorman@gmail.com>
2025-12-09 00:07:55 -0800

    added sched -> fdr struct and params

--- dice_new/rtl/frontend_pkg.sv
+++ dice_new/rtl/frontend_pkg.sv
@@ -7,10 +7,23 @@ I have not researched them yet but am initializing this file for the meta_fetch 
 
 
 package frontend_pkg;
+
+  // =========================================================
+  // PARAMETERS
+  // =========================================================
+  localparam MAX_NUM_CTA     = 4;
+  localparam MAX_NUM_THREADS = 32; 
+  localparam PC_WIDTH        = 32;
+  
+  // Derived Parameters (Calculated automatically)
+  localparam CTA_ID_WIDTH    = $clog2(MAX_NUM_CTA);
+  localparam MAX_EBLOCK      = MAX_NUM_CTA + 4; 
+  localparam EBLOCK_ID_WIDTH = $clog2(MAX_EBLOCK);
+  localparam THREAD_WIDTH    = MAX_NUM_THREADS;
   // =========================================================
   // Type definitions
   // =========================================================
-    typedef struct packed {
+  typedef struct packed {
     logic [31:0]     bitstream_addr;     // 32-bit: Address of the CGRA configuration bitstream
     logic [7:0]      bitstream_length;   // 8-bit: Bitstream size in bytes
     logic [1:0]      unrolling_factor;   // 2-bit: Max thread unrolling factor
@@ -22,6 +35,16 @@ package frontend_pkg;
     logic [31:0]     branch_meta;        // 32-bit: Branch/Jump metadata
     logic            barrier;            // 1-bit: Barrier indicator (must wait for previous blocks)
     logic            parameter_load;     // 1-bit: True if p-graph only loads constants
-    } pgraph_meta_t;
-    
+  } pgraph_meta_t;
+
+
+  typedef struct packed {
+    logic [CTA_ID_WIDTH-1:0]      schedule_hw_cta_id; //cta id
+    logic [PC_WIDTH-1:0]          schedule_next_pc; //pc
+    logic [EBLOCK_ID_WIDTH-1:0]   schedule_eblock_id; //e block id
+    logic                         schedule_cta_predicted; //prefetch?
+    logic [THREAD_WIDTH-1:0]      active_mask;
+    dice_kernel_desc_t            kernel_info; //MAY NEED TO BE CHANGED
+  } schedule_t;
+  
 endpackage
