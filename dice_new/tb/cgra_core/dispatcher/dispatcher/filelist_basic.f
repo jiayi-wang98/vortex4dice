05797332e75a7a93be6e0d790affd1d5d06c461f
Juwon Jun <jami3jun@squire.ece.uw.edu>
2025-12-16 18:54:35 -0800

    next_thread_logic testbench changes

--- dice_new/tb/cgra_core/dispatcher/dispatcher/filelist_basic.f
+++ dice_new/tb/cgra_core/dispatcher/dispatcher/filelist_basic.f
@@ -6,7 +6,9 @@ $DICE_HOME/rtl/cgra_core/dispatcher/priority_encoder_8bit.sv
 $DICE_HOME/rtl/cgra_core/dispatcher/priority_encoder_64bit.sv
 $DICE_HOME/rtl/cgra_core/dispatcher/reverse_mapper.sv
 $DICE_HOME/rtl/cgra_core/dispatcher/thread_filter.sv
+$DICE_HOME/rtl/cgra_core/dispatcher/thread_lane_reroute.sv
 $DICE_HOME/rtl/cgra_core/dispatcher/sync_fifo.sv
+$DICE_HOME/rtl/cgra_core/dispatcher/sync_fifo_read_unreg.sv
 $DICE_HOME/rtl/cgra_core/dispatcher/dispatcher.sv
 $DICE_HOME/rtl/cgra_core/dispatcher/constant_scoreboard.sv
 $DICE_HOME/tb/cgra_core/dispatcher/dispatcher/tb_basic_dispatcher.sv
