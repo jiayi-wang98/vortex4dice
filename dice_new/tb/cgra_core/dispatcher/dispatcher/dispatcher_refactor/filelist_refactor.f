9883782a663f7f185a4f16c0a32c994ffb317460
Juwon Jun <jami3jun@squire.ece.uw.edu>
2026-01-07 22:58:54 -0800

    update to file name

--- dice_new/tb/cgra_core/dispatcher/dispatcher/dispatcher_refactor/filelist_refactor.f
+++ dice_new/tb/cgra_core/dispatcher/dispatcher/dispatcher_refactor/filelist_refactor.f
@@ -11,7 +11,7 @@ $DICE_HOME/rtl/cgra_core/dispatcher/thread_lane_reroute.sv
 $DICE_HOME/rtl/cgra_core/dispatcher/sync_fifo.sv
 $DICE_HOME/rtl/cgra_core/dispatcher/sync_fifo_read_unreg.sv
 $DICE_HOME/rtl/cgra_core/dispatcher/constant_scoreboard.sv
-$DICE_HOME/rtl/cgra_core/dispatcher/dispatcher_refactor/dispatcher_new.sv
+$DICE_HOME/rtl/cgra_core/dispatcher/dispatcher_refactor/dispatcher_refactored.sv
 $DICE_HOME/tb/cgra_core/dispatcher/dispatcher/dispatcher_refactor/tb_refactored_dispatcher.sv
 $DICE_HOME/rtl/cgra_core/dispatcher/dispatcher_refactor/dispatcher_fsm.sv
 $DICE_HOME/rtl/cgra_core/dispatcher/dispatcher_refactor/dispatcher_ctrl.sv
